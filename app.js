65365c10b8b33a1b7d4005035434ebf00c880f38
OmarBknd <omar.bknd@gmail.com>
2025-02-09 18:06:39 +0100

    add index route

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const passport = require('./config/passport-jwt'); 
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(passport.initialize())
+
+const indexRouter = require('./routes/indexRouter');
+const signUpRouter = require('./routes/signUpRouter');
+const signInRouter = require('./routes/signInRouter')
+const postRouter = require('./routes/postRouter');
 
 
-app.get('/', (req, res) => {    
-    res.send('Hello World');
-})
+app.use('/', indexRouter);
+app.use('/signup', signUpRouter);
+app.use('/signin', signInRouter);
+app.use('/post', postRouter);
 
 app.listen(port, () => {
     console.log('Server is running on port 3000');
