cf6b30420ec25d693e283c2d4b6fc941185a065a
OmarBknd <omar.bknd@gmail.com>
2025-02-09 20:16:07 +0100

    add comment routes

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const router = Router();
 const passport = require('passport');
 const postController = require('../controllers/postController');
+const commentController = require('../controllers/commentController');
 
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     res.send('Protected Route');
@@ -9,5 +10,10 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 
 router.post('/create', passport.authenticate('jwt', {session: false}), postController.postCreate);
 router.put('/update/:id', passport.authenticate('jwt', {session: false}), postController.postUpdate);
+
+//Comments
+
+router.post('/comment/create', passport.authenticate('jwt', {session: false}), commentController.commentCreate);
+router.delete('/comment/delete/:id', passport.authenticate('jwt', {session: false}), commentController.commentDelete);
 
 module.exports = router;
