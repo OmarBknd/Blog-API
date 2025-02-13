a21c44b3892e92a14e33a6b0ac97369bc6f64fc0
OmarBknd <omar.bknd@gmail.com>
2025-02-13 02:03:32 +0100

    display the user name

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,9 +1,36 @@
 import {PostList} from '../components/posts'
-
+import { getProfile } from '../api';
+import { useEffect, useState } from 'react';
 const Home = () => {
 
+    type User = {
+        firstName : string;
+        lastName : string;
+    }
+const [user, setUser] = useState<User | null>(null)
+const userId = localStorage.getItem('userId')
+
+useEffect(() => {
+    const fetchUser = async () => {
+        if (!userId) return;
+
+        try {
+            const data = await getProfile(userId);
+            setUser(data.user);
+        } catch (error) {
+            console.error("Error fetching user:", error);
+        }
+    };
+
+    fetchUser();
+}, [userId]);
+
     return(
+        <>
+        <div>{user && <p>{user.firstName}{user.lastName}</p> }</div>
         <PostList/>
+        </>
+        
     )
 }
 
