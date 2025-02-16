227f3098a0d229424319c1d9038cdd1884436619
OmarBknd <omar.bknd@gmail.com>
2025-02-16 18:07:30 +0100

    moved handle functions

--- frontend/src/components/posts-management/PostsList.tsx
+++ frontend/src/components/posts-management/PostsList.tsx
@@ -1,40 +1,11 @@
-import { useState, useEffect } from "react";
+
 import { getPosts } from "../../api";
+import { usePost } from "../../hooks/usePost";
 import PostCard from "./PostCard";
-import { Post } from "../../types";
+
 const PostList = () => {
- 
+  const { posts, handleCommentDelete, handlePostDelete } = usePost(getPosts);
 
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const data = await getPosts();
-        setPosts(data.posts);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-      }
-    };
-    fetchPosts();
-  }, []);
-
-  const handleCommentDelete = (postId: string, commentId: string) => {
-    setPosts((prevPosts) =>
-      prevPosts.map((post) =>
-        post.id === postId
-          ? { ...post, comments: post.comments.filter((c) => c.id !== commentId) }
-          : post
-      )
-    );
-  };
-
-  const handlePostDelete = (postId: string) => {
-    
-    setPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
-  };
- 
-  
   return (
     <div className="max-w-4xl mx-auto p-6 bg-gray-50 rounded-lg shadow-md">
       <h1 className="text-3xl font-bold text-gray-800 mb-8 text-center">Latest Posts</h1>
