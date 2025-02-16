d89ffe607baef7b10342b36eb0926db18909a992
OmarBknd <omar.bknd@gmail.com>
2025-02-16 01:41:07 +0100

    seperate comments api

---
+++ frontend/src/api/comment.ts
@@ -0,0 +1,36 @@
+import api from "./api"
+
+type Comment = {
+    content : string
+    
+}
+const commentCreate = async (commentData : Comment, postId:string ) =>{
+    try{
+        const token = localStorage.getItem('token')
+        const response = await api.post(`/post/${postId}/comment/create`, commentData,{
+            headers: {Authorization:`Bearer ${token}`}
+        })
+        
+        return response.data
+    }catch(error) {
+        console.error('Error creating comment', error);
+        
+    }
+}
+
+const commentDelete = async( commentId:string) =>{
+    try{
+        const token = localStorage.getItem('token')
+        const response = await api.delete(`/post/comment/delete/${commentId}`,{
+            headers: {Authorization:`Bearer ${token}`}
+        })
+        
+        return response.data
+    }catch(error) {
+        console.error('Error deleting comment', error);
+        
+    }
+}
+
+
+export {commentCreate, commentDelete,}
