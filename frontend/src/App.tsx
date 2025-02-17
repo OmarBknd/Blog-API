c6c4a88d7926b3287f1e7d37ba19f4623c3691a0
OmarBknd <omar.bknd@gmail.com>
2025-02-17 04:58:27 +0100

    add post by id route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-import CommentCreate from "./components/Comment"
+
 import Navbar from "./components/Navbar"
 import PasswordChange from "./components/PasswordChange"
-import PostCreate from "./components/PostCreate"
-import Home from "./pages/home"
+import PostCreate from "./components/posts-management/PostCreate"
+import PostDetail from "./components/posts-management/PostDetail"
+import PostUpdate from "./components/posts-management/PostUpdate"
+import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
@@ -17,8 +19,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile/:id" element={<Profile/>}/>
         <Route path="/profile/:id/change-password" element={<PasswordChange/>}/>
+        <Route path="/post/:postId" element={<PostDetail/>}/>
         <Route path="/post/create" element={<PostCreate/>}/>
-        <Route path="/post/:postId/comment/create" element={<CommentCreate/>}/>
+        <Route path="/post/update/:postId" element={<PostUpdate/>}/>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
