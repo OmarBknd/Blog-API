279151f067251cfde3eefebb73376dae165f821d
OmarBknd <omar.bknd@gmail.com>
2025-02-17 05:01:02 +0100

    seperate post logic from content

--- frontend/src/components/posts-management/PostsList.tsx
+++ frontend/src/components/posts-management/PostsList.tsx
@@ -1,18 +1,17 @@
-
 import { getPosts } from "../../api";
 import { usePost } from "../../hooks/usePost";
 import PostCard from "./PostCard";
 
 const PostList = () => {
-  const { posts, handleCommentDelete, handlePostDelete } = usePost(getPosts);
+  const { posts } = usePost(getPosts);
 
   return (
     <div className="max-w-4xl mx-auto p-6 bg-gray-50 rounded-lg shadow-md">
       <h1 className="text-3xl font-bold text-gray-800 mb-8 text-center">Latest Posts</h1>
-      <ul className="space-y-8">
+      <ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {posts.map((post) => (
           <li key={post.id}>
-            <PostCard post={post} onCommentDelete={handleCommentDelete} onPostDelete={handlePostDelete} />
+            <PostCard post={post} />
           </li>
         ))}
       </ul>
@@ -20,4 +19,4 @@ const PostList = () => {
   );
 };
 
-export { PostList };
+export default PostList;
