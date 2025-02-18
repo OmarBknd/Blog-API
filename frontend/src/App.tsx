e35420212505ed8b28daf9755f6f026b52c2a262
OmarBknd <omar.bknd@gmail.com>
2025-02-18 03:01:11 +0100

    add admin routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,15 @@ import PasswordChange from "./components/PasswordChange"
 import PostCreate from "./components/posts-management/PostCreate"
 import PostDetail from "./components/posts-management/PostDetail"
 import PostUpdate from "./components/posts-management/PostUpdate"
+import AdminIndex from "./pages/admin/AdminIndex"
 import Home from "./pages/Home"
 import Profile from "./pages/Profile"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import AdminComments from "./pages/admin/AdminComments"
+import AdminPosts from "./pages/admin/AdminPosts"
+import AdminUsers from "./pages/admin/AdminUsers"
 
 
 function App() {
@@ -17,6 +21,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin-dashboard" element={<AdminIndex/>} />
+        <Route path="/admin-dashboard/users" element={<AdminUsers/>} />
+        <Route path="/admin-dashboard/posts" element={<AdminPosts/>} />
+        <Route path="/admin-dashboard/comments" element={<AdminComments/>} />
         <Route path="/profile/:id" element={<Profile/>}/>
         <Route path="/profile/:id/change-password" element={<PasswordChange/>}/>
         <Route path="/post/:postId" element={<PostDetail/>}/>
