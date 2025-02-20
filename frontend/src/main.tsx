df0425da04e88ef4c2de5d932110ce4d98442392
OmarBknd <omar.bknd@gmail.com>
2025-02-20 02:12:11 +0100

    wrap the app with the theme provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import {ThemeProvider} from './components/DarkTheme'
 import './index.css'
 
 import App from './App'
@@ -9,6 +9,9 @@ import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider>
     <App/>
+    </ThemeProvider>
+    
   </StrictMode>,
 )
