52cb7c32c04597ce58bb2c5bbc94c7e9cb48643c
OmarBknd <omar.bknd@gmail.com>
2025-02-22 02:13:02 +0100

    add createdat type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,6 +1,7 @@
 export type Comment = {
     id: string;
     content: string;
+    createdAt: string
     author: {
       firstName: string;
       lastName: string;
@@ -12,10 +13,20 @@ export type Post = {
     id: string;
     title: string;
     content: string;
+    createdAt:string
     comments: Comment[];
+    published:boolean;
     author: {
       firstName: string;
       lastName: string;
       id:string;
     };
   };
+
+export  type User = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    createdAt:string;
+  }
