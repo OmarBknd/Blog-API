e0d368edc823b091b3f6c8c81d40acd86e14ec6c
OmarBknd <omar.bknd@gmail.com>
2025-02-23 04:56:44 +0100

    move api url to .env

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+const apiUrL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: apiUrL,
     headers: {
         'Content-Type': 'application/json'
     }
