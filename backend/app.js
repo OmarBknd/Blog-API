054df2f4ba1810a3d4ef6c608a0182da73d438dc
OmarBknd <omar.bknd@gmail.com>
2025-02-24 00:57:13 +0100

    add host

--- backend/app.js
+++ backend/app.js
@@ -31,6 +31,6 @@ app.use('/signup', signUpRouter);
 app.use('/signin', signInRouter);
 app.use('/post', postRouter);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log('Server is running on port 3000');
 });
